986d902eb8d579995b52c39b302255998ff1786f
Robert Laws <roblaws@me.com>
2018-03-25 20:40:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,9 @@ function setBestCustomer() {
 function overwriteBestCustomer() {
   bestCustomer = "maybe bob"
 }
+
+const leastFavoriteCustomer = "hal"
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "kal"
+}
